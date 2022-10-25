ba30076585723486d72926dddf331792b6bcafa9
hyeongjin <tkdlqh2@hanmail.net>
2022-10-26 01:27:12 +0900

    fix: ChatRoomMySqlRepository의 method에 조건 추가
    privateRoom == false
    deletedAt == null

--- src/main/java/com/example/teamrocket/chatRoom/repository/mysql/ChatRoomMySqlRepository.java
+++ src/main/java/com/example/teamrocket/chatRoom/repository/mysql/ChatRoomMySqlRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomMySqlRepository extends JpaRepository<ChatRoomMySql,String> {
-    Page<ChatRoomMySql> findByRcate1OrderByStart_date(String rcate1, String start_date,Pageable pageable);
-    Page<ChatRoomMySql> findByRcate1AndRcate2OrderByStart_date(String rcate1, String rcate2, Pageable pageable);
+    Page<ChatRoomMySql> findAllByRcate1AndPrivateRoomFalseAndDeletedAtIsNullOrderByStart_date(String rcate1, Pageable pageable);
+    Page<ChatRoomMySql> findAllByRcate1AndRcate2AndPrivateRoomFalseAndDeletedAtIsNullOrderByStart_date(String rcate1, String rcate2, Pageable pageable);
 }
