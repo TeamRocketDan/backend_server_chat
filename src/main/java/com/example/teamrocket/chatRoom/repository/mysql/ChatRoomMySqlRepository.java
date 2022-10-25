bd3c5525e64dffc81c037bcecc12b7c14dd9e7e0
hyeongjin <tkdlqh2@hanmail.net>
2022-10-26 01:09:56 +0900

    ChatRoomMySqlRepository 에 pageable 추가

--- src/main/java/com/example/teamrocket/chatRoom/repository/mysql/ChatRoomMySqlRepository.java
+++ src/main/java/com/example/teamrocket/chatRoom/repository/mysql/ChatRoomMySqlRepository.java
@@ -1,7 +1,11 @@
 package com.example.teamrocket.chatRoom.repository.mysql;
 
 import com.example.teamrocket.chatRoom.entity.mysql.ChatRoomMySql;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomMySqlRepository extends JpaRepository<ChatRoomMySql,String> {
+    Page<ChatRoomMySql> findByRcate1OrderByStart_date(String rcate1, String start_date,Pageable pageable);
+    Page<ChatRoomMySql> findByRcate1AndRcate2OrderByStart_date(String rcate1, String rcate2, Pageable pageable);
 }
