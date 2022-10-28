4a391f99e4895ece2d0c0061fd062ed684b85916
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2022-10-28 18:23:11 +0900

    feat: MessageRepository 추가 & MessagePagingResponse 추가

---
+++ src/main/java/com/example/teamrocket/utils/MessagePagingResponse.java
@@ -0,0 +1,52 @@
+package com.example.teamrocket.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MessagePagingResponse<Message> {
+
+    private boolean lastDay;
+    private LocalDate targetDay;
+
+    private boolean firstPage;
+    private boolean lastPage;
+    private int targetDayTotalPage;
+    private long targetDayTotalElements;
+    private int size;
+    private int targetDayCurrentPage;
+    private List<Message> content;
+
+    public void setFromPage(Page<Message> page,LocalDate targetDay) {
+
+        this.targetDay = targetDay;
+
+        this.firstPage = page.isFirst();
+        this.lastPage = page.isLast();
+        this.targetDayTotalPage = page.getTotalPages();
+        this.targetDayTotalElements = page.getTotalElements();
+        this.size = page.getSize();
+        this.targetDayCurrentPage = page.getNumber();
+        this.content = page.getContent();
+    }
+
+    public void setLastDay(boolean isLastDay){
+        this.lastDay = isLastDay;
+    }
+
+    public void setFromList(List<Message> list, Integer size,LocalDate targetDay){
+        this.targetDay = targetDay;
+        this.content = list;
+        this.lastPage = list.size() != size;
+
+    }
+}
