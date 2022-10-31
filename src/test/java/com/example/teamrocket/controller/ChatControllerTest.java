046a3c3aa4441354630876fdb317f323775f7594
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2022-10-31 17:45:55 +0900

    test : createRoom 추가

--- src/test/java/com/example/teamrocket/controller/ChatControllerTest.java
+++ src/test/java/com/example/teamrocket/controller/ChatControllerTest.java
@@ -5,25 +5,24 @@ import com.example.teamrocket.chatRoom.domain.ChatRoomDto;
 import com.example.teamrocket.error.exception.ChatRoomException;
 import com.example.teamrocket.error.exception.UserException;
 import com.example.teamrocket.service.ChatService;
-import com.example.teamrocket.service.ChatServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.*;
+import static com.example.teamrocket.error.type.ChatRoomErrorCode.TRAVEL_START_DATE_MUST_BE_BEFORE_END_DATE;
+import static com.example.teamrocket.error.type.UserErrorCode.USER_NOT_FOUND;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -45,7 +44,7 @@ public class ChatControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
-    void successCreateRoom() throws Exception{
+    void createRoomSuccess() throws Exception{
 
         given(chatService.createRoom(anyLong(),any())).willReturn(
                 ChatRoomDto.builder()
@@ -65,4 +64,42 @@ public class ChatControllerTest {
 
     }
 
+    @Test
+    void createRoomFail_UserException() throws Exception{
+
+        doThrow(new UserException(USER_NOT_FOUND))
+                .when(chatService).createRoom(any(),any());
+
+        mockMvc.perform(post("/api/v1/chat/room")
+                        .header("X_AUTH_TOKEN","111")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new ChatRoomCreateInput()
+                        )))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.result").isEmpty())
+                .andExpect(jsonPath("$.errorMessage").value(USER_NOT_FOUND.getMessage()))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("여행 시작날짜 끝날짜 순서 에러")
+    void createRoomFail_TravelDateError() throws Exception{
+
+        doThrow(new ChatRoomException(TRAVEL_START_DATE_MUST_BE_BEFORE_END_DATE))
+                .when(chatService).createRoom(any(),any());
+
+        mockMvc.perform(post("/api/v1/chat/room")
+                        .header("X_AUTH_TOKEN","111")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new ChatRoomCreateInput()
+                        )))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.result").isEmpty())
+                .andExpect(jsonPath("$.errorMessage").value(TRAVEL_START_DATE_MUST_BE_BEFORE_END_DATE.getMessage()))
+                .andDo(print());
+    }
 }
