f7ea444d2d554907646c75a9db20b8fae833f1d0
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2022-11-08 20:58:29 +0900

    feat : ChatRoomPartipantRepository에 findAllByChatRoomMySql 추가

--- src/main/java/com/example/teamrocket/chatRoom/repository/mysql/ChatRoomParticipantRepository.java
+++ src/main/java/com/example/teamrocket/chatRoom/repository/mysql/ChatRoomParticipantRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChatRoomParticipantRepository extends JpaRepository<ChatRoomParticipant,Long> {
     Optional<ChatRoomParticipant> findByChatRoomMySqlAndUserId(ChatRoomMySql chatRoomMySql, Long UserId);
     Page<ChatRoomParticipant> findAllByUserId(Long userId, Pageable pageable);
     void deleteAllByChatRoomMySql(ChatRoomMySql chatRoomMySql);
+    List<ChatRoomParticipant> findAllByChatRoomMySql(ChatRoomMySql chatRoom);
 }
