2f92468f71b573cd92d87ddd16a5d7cc1129a92e
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2022-11-08 20:45:28 +0900

    feat : ChatRoomMySqlRepository에서 chatRoomList 불러올 떄, EndDateBefore 조건 추가

--- src/main/java/com/example/teamrocket/chatRoom/repository/mysql/ChatRoomMySqlRepository.java
+++ src/main/java/com/example/teamrocket/chatRoom/repository/mysql/ChatRoomMySqlRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface ChatRoomMySqlRepository extends JpaRepository<ChatRoomMySql,String> {
-    Page<ChatRoomMySql> findAllByRcate1AndRcate2AndDeletedAtIsNullOrderByStartDate(String rcate1, String rcate2, Pageable pageable);
+    Page<ChatRoomMySql> findAllByRcate1AndRcate2AndDeletedAtIsNullAndEndDateBeforeOrderByStartDate(String rcate1, String rcate2, LocalDate endDate, Pageable pageable);
     Optional<ChatRoomMySql> findByIdAndDeletedAtIsNull(String id);
 }
