e31a07629c3ac2ca9afe726b0f96bb7c501770bc
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2022-11-17 15:32:30 +0900

    feat: setMessageCount 추가

--- src/main/java/com/example/teamrocket/chatRoom/entity/DayOfMessages.java
+++ src/main/java/com/example/teamrocket/chatRoom/entity/DayOfMessages.java
@@ -22,4 +22,8 @@ public class DayOfMessages {
     @DocumentReference
     private List<Message> messages;
     private int messagesCount;
+
+    public void setMessagesCount(int messagesCount){
+        this.messagesCount = messagesCount;
+    }
 }
